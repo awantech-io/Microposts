c111e44e3a119062b78821ea1002cf61c2f6c28a
malaynium <hello.wanlab@gmail.com>
2020-09-12 11:59:33 +0800

    delete post

--- src/app.js
+++ src/app.js
@@ -8,6 +8,9 @@ document.addEventListener('DOMContentLoaded', getPosts);
 // Listen for add Post
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+// Listen for delete Post
+document.querySelector('#posts').addEventListener('click', deletePost);
+
 // Get Posts
 function getPosts() {
   http
@@ -38,4 +41,28 @@ function submitPost() {
       getPosts();
     })
     .catch((err) => console.log(err));
+}
+
+// Delete Post
+function deletePost(e) {
+  e.preventDefault();
+  // check event
+  if (e.target.parentElement.classList.contains('delete')) {
+    // get id
+    const id = e.target.parentElement.dataset.id;
+    // prompt alert
+    if (confirm('Are you sure?')) {
+      // delete the post
+      http
+        .delete(`http://localhost:3000/posts/${id}`)
+        .then((data) => {
+          // show success msg
+          ui.showAlert('Post Removed', 'alert alert-success');
+          // display remaining post
+          getPosts();
+        })
+        // catch eror if any
+        .catch((err) => console.log(err));
+    }
+  }
 }
