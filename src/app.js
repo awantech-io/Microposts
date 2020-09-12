4d5dde9ee9957b4615f7c80727448d25bc9cc4b9
malaynium <hello.wanlab@gmail.com>
2020-09-12 10:58:52 +0800

    Add Posts features

--- src/app.js
+++ src/app.js
@@ -5,10 +5,35 @@ import { ui } from './ui';
 // get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Listen for add Post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+// Get Posts
 function getPosts() {
   http
     .get('http://localhost:3000/posts')
     .then((data) => ui.showPosts(data))
     //.then((data) => console.log(data))
+    .catch((err) => console.log(err));
+}
+
+// Submit Post
+function submitPost() {
+  // get form data
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  // change data to object literal
+  const data = {
+    title,
+    body,
+  };
+
+  // Create Post
+  http
+    .post('http://localhost:3000/posts', data)
+    .then((data) => {
+      getPosts();
+    })
     .catch((err) => console.log(err));
 }
